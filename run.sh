f887f843d84bcaf44e9f046245d5a307aa1c8253
kclary <kclary@cs.umass.edu>
2018-10-18 16:47:28 -0400

    modify loop structure

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ envs="BreakoutNoFrameskip-v0"
 algs="acer a2c trpo_mpi ppo2 deepq"
 timesteps="3e6 1e7"
 work1=/mnt/nfs/work1/jensen/kclary
-iters="1 2 3 4 5 6 7 8 9 10"
+iters="0 1 2 3 4 5 6 7 8 9"
 
 # make sure we have all the pip dependencies we want installed
 pip3 install gym[atari] --user
@@ -42,10 +42,10 @@ for steps in $timesteps; do
 	python3 $runner $iftb --alg=$alg --env=$env --num_timesteps=$steps --save_path=$model"
 			    echo "$cmd"
 			    echo "$cmd" > $dest
-			    #sbatch -p $partition --gres=gpu:1 $dest
+			    sbatch -p $partition --gres=gpu:1 $dest
 		    done;
-	    exit
+	    #exit
 		done;
-	exit
+	#exit
     done;
 done
