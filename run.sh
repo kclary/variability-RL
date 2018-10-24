b8eb14d708f133bf7198e3529fa2245b5fd5865e
kclary <kclary@cs.umass.edu>
2018-10-24 15:39:17 -0400

    update to baselines benchmark exps

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
-envs="BreakoutNoFrameskip-v0"
-algs="acer a2c trpo_mpi ppo2 deepq"
-timesteps="3e6 1e7"
+envs="BreakoutNoFrameskip-v4 PongNoFrameksip-v4 EnduroNoFrameskip-v4 QbertNoFrameskip-v4 BeamRiderNoFrameskip-v4 SeaquestNoFrameskip-v4 SpaceInvadersNoFrameskip-v4"
+algs="acer a2c ppo2 deepq acktr"
+timesteps="1e7"
 work1=/mnt/nfs/work1/jensen/kclary
 iters="0 1 2 3 4 5 6 7 8 9"
+iters="0"
 
 # make sure we have all the pip dependencies we want installed
 pip3 install gym[atari] --user
@@ -28,7 +29,7 @@ for steps in $timesteps; do
 			    else
 				partition="titanx-long"
 			    fi
-                         
+         
 			    uid=$env.$alg.$steps.$iter
 			    dest=scripts/run_cmd_$uid.sbatch
 
@@ -37,8 +38,8 @@ for steps in $timesteps; do
 			    cmd="#!/bin/bash
 	#
 	#SBATCH --job-name=$uid
-	#SBATCH --output=$uid.out
-	#SBATCH -e $uid.err
+	#SBATCH --output=$alg.$uid.out
+	#SBATCH -e $alg.$uid.err
 	#SBATCH --mem=16g
 
 
